c39176881ea45e6b103f36e440002d64659058a9
gatech0723 <shawnk723@live.com>
2020-02-24 21:50:22 -0800

    Updates

---
+++ precache-manifest.1b5ff1c6e7cf8121d5f7c95afe2e4e31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ae89542210ed6915e35d1ac46a3fca",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f386ce3123ce729bf1be",
+    "url": "/robofriends/static/css/main.9e5d9033.chunk.css"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/robofriends/static/js/2.4b6b2329.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.4b6b2329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f386ce3123ce729bf1be",
+    "url": "/robofriends/static/js/main.447c174d.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
