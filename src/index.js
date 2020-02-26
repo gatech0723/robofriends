5b4797dae6156f7b14587afe264c28c50a1517fd
gatech0723 <shawnk723@live.com>
2020-02-26 00:12:57 -0800

    added provider

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const store = createStore(searchRobots)
 ReactDOM.render(
   <Provider store={store}>
   <App />
+  
   </Provider>, document.getElementById('root'));
   
 // If you want your app to work offline and load faster, you can change
